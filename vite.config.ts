e7d4f45b5fadd6c621a2604c6df9f51242509dc9
Jefferson Campos <contato@jeffersonjunior.com.br>
2026-06-03 15:11:34 -0300

    perf: optimize client bundle size using manual vendor chunks to resolve bundle warning

--- vite.config.ts
+++ vite.config.ts
@@ -10,4 +10,24 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            if (id.includes('react') || id.includes('scheduler')) {
+              return 'vendor-react';
+            }
+            if (id.includes('lucide-react')) {
+              return 'vendor-lucide';
+            }
+            if (id.includes('framer-motion')) {
+              return 'vendor-motion';
+            }
+            return 'vendor-others';
+          }
+        }
+      }
+    }
+  }
 })

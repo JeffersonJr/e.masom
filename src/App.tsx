06260d1b4eb11bd8bda1344a462fefe4f87b5b3a
Jefferson Campos <contato@jeffersonjunior.com.br>
2026-06-05 14:28:50 -0300

    feat: integrate Vercel Analytics

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,14 @@
 
 import AppRouter from './routes';
+import { Analytics } from '@vercel/analytics/react';
 
 function App() {
-  return <AppRouter />;
+  return (
+    <>
+      <AppRouter />
+      <Analytics />
+    </>
+  );
 }
 
 export default App;
